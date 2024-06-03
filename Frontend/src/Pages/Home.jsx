87edb0b428866919d1b5ffc5490daa2966789d71
smit jogani <smitjogani00@gmail.com>
2024-06-03 21:55:23 +0530

    UI;

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -2,8 +2,19 @@ import Repos from "../Components/Repos"
 import Search from "../Components/Search"
 import SortRepo from "../Components/SortRepo"
 import ProfileInfo from "../Components/ProfileInfo"
+import { useEffect, useState } from "react"
 
 const Home = () => {
+
+  const [userProfile, setUserProfile] = useState(null);
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [sortType, swetSortType] = useState("forks");
+
+  useEffect(() => {
+    
+  },[])
+
   return (
     <div className="m-4">
       <Search/>
